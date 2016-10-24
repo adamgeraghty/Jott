c2b1d2b62e157b78fedb1ae804bb6bcd5c7f241a
Adam Geraghty <babydocfile@gmail.com>
2016-10-24 10:22:12 +0100

    first commit

---
+++ app/src/main/java/com/adamgeraghty/jott/fragment/NoteLinedEditorFragment.java
@@ -0,0 +1,27 @@
+package com.adamgeraghty.jott.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adamgeraghty.jott.R;
+
+public class NoteLinedEditorFragment extends Fragment {
+
+
+    public NoteLinedEditorFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_lined_editor, container, false);
+    }
+}
